42e633f0a431bfb5f4263dcaf3ce16659d3215b4
brad <brad@brad.tw>
2017-01-21 14:18:03 +0800

    v3

--- app/src/main/java/tw/org/iii/brad09/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad09/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private SimpleAdapter adapter;
+    private LinkedList<HashMap<String,String>> data;
+    private String[] from = {"brad1","brad2"};
+    private int[] to = {R.id.item_title, R.id.item_content};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +25,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initListView(){
+        data = new LinkedList<>();
 
-        adapter = new SimpleAdapter(this,data,R.layout.activity_main,from,to);
-        listView.setAdapter();
+        HashMap<String,String> d0 = new HashMap<>();
+        d0.put(from[0], "data 0");
+        d0.put(from[1], "cont 0");
+        data.add(d0);
+
+        HashMap<String,String> d1 = new HashMap<>();
+        d1.put(from[0], "data 1");
+        d1.put(from[1], "cont 1");
+        data.add(d1);
+
+        HashMap<String,String> d2 = new HashMap<>();
+        d2.put(from[0], "data 2");
+        d2.put(from[1], "cont 2");
+        data.add(d2);
+
+        adapter = new SimpleAdapter(this,data,R.layout.listitem,
+                from,to);
+        listView.setAdapter(adapter);
 
 
     }
