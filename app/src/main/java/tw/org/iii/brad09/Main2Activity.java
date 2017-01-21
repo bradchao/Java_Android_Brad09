82ea0342c9768e84ec8f33cb54428c925cee4a8c
brad <brad@brad.tw>
2017-01-21 15:24:10 +0800

    v8

--- app/src/main/java/tw/org/iii/brad09/Main2Activity.java
+++ app/src/main/java/tw/org/iii/brad09/Main2Activity.java
@@ -1,18 +1,72 @@
 package tw.org.iii.brad09;
 
-import android.support.v7.app.AlertController;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
     private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<String> dataset = new LinkedList<String>();
+        for (int i = 0; i < 100; i++){
+            dataset.add("item" + i);
+        }
+
+        MyAdapter adapter = new MyAdapter(dataset);
+        recyclerView.setAdapter(adapter);
 
     }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+
+        public List<String> mDataset;
+
+        public MyAdapter(List<String> dataset) {
+            super();
+            mDataset = dataset;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View view = View.inflate(viewGroup.getContext(), android.R.layout.simple_list_item_1, null);
+            ViewHolder holder = new ViewHolder(view);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, int i) {
+            viewHolder.mTextView.setText(mDataset.get(i));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataset.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder{
+
+            public TextView mTextView;
+            public ViewHolder(View itemView) {
+                super(itemView);
+                mTextView = (TextView) itemView;
+            }
+        }
+    }
+
 }
